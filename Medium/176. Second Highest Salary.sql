e47adc51e33eb755a26579e4019454e950f26daf
Abhijeet Rajput <rajputabhi0912@gmail.com>
2023-09-25 14:17:50 +0530

    176. Second Highest Salary

---
+++ Medium/176. Second Highest Salary.sql
@@ -0,0 +1,81 @@
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | salary      | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains information about the salary of an employee.
+ 
+
+-- Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+-- Example 2:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | null                |
+-- +---------------------+
+
+
+
+
+-- # Write your MySQL query statement below
+-- solution 1
+select case when salary = -1 then null
+            else salary end 
+ as SecondHighestSalary
+from 
+(
+select id, salary from Employee
+union
+select 0 id, -1 salary
+) a
+where salary < (
+  select max(e.salary)
+  from Employee as e
+)
+order by 1 desc
+limit 1
+;
+
+
+
+
+
+-- solution 2
+select
+(select distinct Salary 
+from Employee order by salary desc 
+limit 1 offset 1) 
+as SecondHighestSalary;
